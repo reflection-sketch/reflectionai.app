99d0cc71c6a0f68e4d90231133598f466385c1c1
Alex yip <sz423242122@gmail.com>
2023-11-22 10:51:52 +0800

    test: theme config update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import 'styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
+import muiTheme from 'provider/MuiThemeProvider'
 import ParticleProvider from 'provider/ParticleProvider'
 import StateProvider from 'provider/StateProvider'
 import ApplicationUpdater from 'state/application/updater'
 import { MulticallUpdater } from 'state/multicall'
-
+import 'styles/globals.css'
 function Updater() {
   return (
     <>
@@ -18,8 +19,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ParticleProvider>
       <StateProvider>
-        <Updater />
-        <Component {...pageProps} />
+        <ThemeProvider theme={muiTheme}>
+          <Updater />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </StateProvider>
     </ParticleProvider>
   )
